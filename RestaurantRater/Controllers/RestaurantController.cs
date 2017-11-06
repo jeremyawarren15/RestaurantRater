fca48f6eef5d72818e77d4086994f448d55d8006
Jeremy Warren <jeremyawarren15@gmail.com>
2017-11-06 15:06:37 -0500

    added detail view functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -104,5 +104,20 @@ namespace RestaurantRater.Controllers
             }
             return View(restaurant);
         }
+
+        // GET: Restaurants/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
